0d140d850c1a92f9cc91b158602f8e11d43e28f4
Kundan Kumar <88768757+KundanKumarRoy@users.noreply.github.com>
2022-08-24 13:59:52 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+// Google chart curving chart
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ['Month', 'Number of user 2022', 'Total Business 2022'],
+    ['Jan',  10,     10],
+    ['Fab',  30,      40],
+    ['March',  45,       50],
+    ['May',  70,      55],
+    ['Jun',  75,     65],
+    ['July',  80,      75],
+    ['Aug',  85,       68],
+    ['Sep',  75,      95]
+  ]);
+
+  var options = {  
+    curveType: 'function',
+    legend: { position: 'top'}
+  };
+
+  var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+  chart.draw(data, options);
+}
+ 
